d01b5ea4ada69805dd7198e8f8b3193e6d1780d2
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-03-16 01:01:42 +0600

    Header Completed

---
+++ src/Header.jsx
@@ -0,0 +1,94 @@
+import profile from "./assets/img/au1.png";
+const Header = () => {
+  return (
+    <div className="flex bg-white items-center justify-center">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl font-bold">Recipe Calories</a>
+        </div>
+        <div className="navbar-center hidden lg:flex gap-4">
+          <div></div>
+          <div>
+            <a href="#">Home</a>
+          </div>
+          <div>
+            <a href="#">Recipes</a>
+          </div>
+          <div>
+            <a href="#">About</a>
+          </div>
+          <div>
+            <a href="#">Search</a>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div className="flex gap-2">
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Search"
+                className="input input-bordered w-24 md:w-auto rounded-full"
+              />
+            </div>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
